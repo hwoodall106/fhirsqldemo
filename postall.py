dea061f7d70af9694a5df616490623a1919f6ec8
hwoodall106 <117471117+hwoodall106@users.noreply.github.com>
2023-04-03 10:25:18 +0100

    Add files via upload

---
+++ postall.py
@@ -0,0 +1,22 @@
+import json
+import requests
+import os
+import time
+
+## url = "http://localhost:52773/csp/healthshare/demo/fhir/r4/"
+url = 'http://52.56.68.107:52773/csp/healthshare/fhir/fhir/r4/'
+
+headers = {'Content-type': 'application/fhir+json'}
+
+directory = r"C:\Users\hwoodall\synthea\output\fhir"
+## directory = r"C:\Users\hwoodall\synthea-us-data\test\dummyoutput" 
+
+for filename in os.listdir(directory):
+    f = os.path.join(directory, filename)
+    if (os.path.isfile(f)):
+        if (os.path.getmtime(f)>=(time.time()-36000)):
+            t = open(f)
+            data = json.load(t)
+            x= requests.post(url,json = data, headers = headers)
+            print(f,x.status_code)
+
