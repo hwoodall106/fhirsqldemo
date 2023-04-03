9859f32fb051bdc1d15f9acd283edae18252f1a6
hwoodall106 <117471117+hwoodall106@users.noreply.github.com>
2023-04-03 14:59:43 +0100

    Update postall.py

--- postall.py
+++ postall.py
@@ -11,10 +11,12 @@ headers = {'Content-type': 'application/fhir+json'}
 directory = r"C:\Users\hwoodall\synthea\output\fhir"
 ## directory = r"C:\Users\hwoodall\synthea-us-data\test\dummyoutput" 
 
+lookback=36000
+
 for filename in os.listdir(directory):
     f = os.path.join(directory, filename)
     if (os.path.isfile(f)):
-        if (os.path.getmtime(f)>=(time.time()-36000)):
+        if (os.path.getmtime(f)>=(time.time()-lookback)):
             t = open(f)
             data = json.load(t)
             x= requests.post(url,json = data, headers = headers)
